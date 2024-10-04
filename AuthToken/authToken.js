4f4b2f3800c5e332095aaba8137d3a023342c6de
jaanus <zaar2213@gmail.com>
2024-10-04 20:34:40 +0300

    refactored some logic

---
+++ AuthToken/authToken.js
@@ -0,0 +1,22 @@
+const fetchToken = async () => {
+  const base64Encode = (login, password) => {
+    return Buffer.from(`${login}:${password}`).toString('base64');
+  };
+  const authHeader = `Basic ${base64Encode(
+    process.env.LOGIN,
+    process.env.PASSWORD
+  )}`;
+  const response = await fetch('https://01.kood.tech/api/auth/signin', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: authHeader,
+    },
+  });
+  if (!response.ok) {
+    throw new Error(`Failed to fetch token: ${response.statusText}`);
+  }
+  return await response.json();
+};
+
+export default fetchToken;
--- main.js
+++ main.js
@@ -4,39 +4,10 @@ dotenv.config();
 
 import hasAuthorization from './HasAuthorization/hasAuthorization.js';
 import restrictToChannels from './RestrictToChannels/restrictToChannels.js';
+import fetchToken from './AuthToken/authToken.js';
+import capitalizeFirstLetter from './Helpers/capitalize.js';
 
-const capitalizeFirstLetter = (string) => {
-  return string.charAt(0).toUpperCase() + string.slice(1);
-};
-
-// school auth
-
-// fetch token
-const fetchToken = async () => {
-  const base64Encode = (login, password) => {
-    return Buffer.from(`${login}:${password}`).toString('base64');
-  };
-
-  const authHeader = `Basic ${base64Encode(
-    process.env.LOGIN,
-    process.env.PASSWORD
-  )}`;
-
-  const response = await fetch('https://01.kood.tech/api/auth/signin', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      Authorization: authHeader,
-    },
-  });
-
-  if (!response.ok) {
-    throw new Error(`Failed to fetch token: ${response.statusText}`);
-  }
-
-  return await response.json();
-};
-
+// init token
 const token = await fetchToken();
 
 // fetch sprint data
@@ -116,7 +87,6 @@ client.on('ready', () => {
 });
 
 client.on('messageCreate', async (message) => {
-
   // restrictions, bot, channel, command, user:
   if (message.author.bot) return;
   if (!restrictToChannels(message)) return;
