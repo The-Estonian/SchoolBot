016085984dd438227df6c8918e17f4793f928829
jaanus <zaar2213@gmail.com>
2024-10-08 20:40:18 +0300

    forgot to run functions asynchronously

--- src/node/main/main.js
+++ src/node/main/main.js
@@ -51,7 +51,7 @@ client.on('messageCreate', async (message) => {
 
 client.on('guildMemberRemove', async (member) => {
   const channel = client.channels.cache.get('1257316921565646878');
-  userLogging(member.user.tag, 'Left the server');
+  await userLogging(member.user.tag, 'Left the server');
   channel.send(
     `\`\`\`Ahh great, someone left! Let's see now... AHA ${member.user.tag} you scoundrel, let me see where i put your credentials... What was the site again, Autolaen24.ee right?\`\`\``
   );
@@ -59,7 +59,7 @@ client.on('guildMemberRemove', async (member) => {
 
 client.on('guildMemberAdd', async (member) => {
   const channel = client.channels.cache.get('1257316921565646878');
-  userLogging(member.user.tag, 'Joined the server');
+  await userLogging(member.user.tag, 'Joined the server');
   channel.send(
     `\`\`\`Welcome to the server, ${member.user.tag} ! Please give me a second while i fetch your credentials to be saved for later. Toodeloo\`\`\``
   );
