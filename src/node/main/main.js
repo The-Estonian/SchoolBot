fab2a5705612c03055404b1a8b4b880254474a6a
jaanus <zaar2213@gmail.com>
2024-10-08 20:08:47 +0300

    added user join/leave logging to the server logs

--- src/node/main/main.js
+++ src/node/main/main.js
@@ -50,4 +50,18 @@ client.on('messageCreate', async (message) => {
   handleAllCommands(message);
 });
 
+client.on('guildMemberRemove', async (member) => {
+  const channel = client.channels.cache.get('1257316921565646878');
+  channel.send(
+    `\`\`\`Ahh great, someone left! Let's see now... AHA ${member.user.tag} you scoundrel, let me see where i put your credentials... What was the site again, Autolaen24.ee right?\`\`\``
+  );
+});
+
+client.on('guildMemberAdd', async (member) => {
+  const channel = client.channels.cache.get('1257316921565646878');
+  channel.send(
+    `\`\`\`Welcome to the server, ${member.user.tag} ! Please give me a second while i fetch your credentials to be saved for later. Toodeloo\`\`\``
+  );
+});
+
 client.login(process.env.DISCORD_TOKEN);
