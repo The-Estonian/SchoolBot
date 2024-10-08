7f75b5f2f262670f59bf173eca0bb41298fcdfd6
jaanus <zaar2213@gmail.com>
2024-10-08 08:18:03 +0300

    refactored main.js even more

---
+++ src/node/main/CommandHandlers/handleAllCommands.js
@@ -0,0 +1,69 @@
+import helpInfo from '../Helpers/helpInfo.js';
+import replyAndClean from '../CleanAfter/replyAndClean.js';
+import handleRemove from './handleRemove.js';
+import handleCreateCrash from './handleCreateCrash.js';
+import handleSprint from './handleSprint.js';
+import handleUserId from './handleUserId.js';
+import handleFirstName from './handleFirstName.js';
+import handleLastName from './handleLastName.js';
+import handleProject from './handleProject.js';
+import fetchToken from '../AuthToken/authToken.js';
+
+// init token and constants
+const token = await fetchToken();
+
+const handleAllCommands = (message) => {
+  const args = message.content.slice(1).trim().split(/ +/);
+  const command = args.shift().toLowerCase();
+
+  switch (command) {
+    // Remove x amount of messages from channel
+    case 'remove':
+      handleRemove(message, args);
+      break;
+
+    // Create an error to test server crash logging to AWS S3 bucket
+    case 'crash':
+      handleCreateCrash(message);
+      break;
+
+    // sprint
+    case 'sprint':
+      handleSprint(message, args, token);
+      break;
+
+    case 'userid':
+      handleUserId(message, args, token);
+      break;
+
+    // first name
+    case 'firstname':
+      handleFirstName(message, args, token);
+      break;
+
+    // last name
+    case 'lastname':
+      handleLastName(message, args, token);
+      break;
+
+    // project
+    case 'project':
+      handleProject(message, args);
+      break;
+
+    // kiitus
+    case 'kiitus':
+      let name = args.shift();
+      let kiitus = `
+${name} sa oled nii tubli! ;)`;
+      replyAndClean(message, `\`\`\`${kiitus}\`\`\``);
+      break;
+
+    // help
+    case 'help':
+      replyAndClean(message, `\`\`\`${helpInfo()}\`\`\``);
+      break;
+  }
+};
+
+export default handleAllCommands;
--- src/node/main/main.js
+++ src/node/main/main.js
@@ -5,20 +5,10 @@ dotenv.config();
 import hasAuthorization from './HasAuthorization/hasAuthorization.js';
 import restrictToChannels from './RestrictToChannels/restrictToChannels.js';
 import fetchToken from './AuthToken/authToken.js';
-import helpInfo from './Helpers/helpInfo.js';
-import replyAndClean from './CleanAfter/replyAndClean.js';
 import logErrorToFile from './ErrorLogging/logError.js';
-import handleRemove from './CommandHandlers/handleRemove.js';
-import handleCreateCrash from './CommandHandlers/handleCreateCrash.js';
-import handleSprint from './CommandHandlers/handleSprint.js';
-import handleUserId from './CommandHandlers/handleUserId.js';
-import handleFirstName from './CommandHandlers/handleFirstName.js';
-import handleLastName from './CommandHandlers/handleLastName.js';
-import handleProject from './CommandHandlers/handleProject.js';
+import handleAllCommands from './CommandHandlers/handleAllCommands.js';
 
-// init token and constants
-const token = await fetchToken();
-
+// global error handling
 process.on('uncaughtException', (error) => {
   console.error('Uncaught Exception:', error);
   logErrorToFile(error).catch((err) =>
@@ -33,11 +23,12 @@ process.on('unhandledRejection', (reason, promise) => {
   );
 });
 
-// bot
+// bot client
 const client = new Client({
   intents: ['Guilds', 'GuildMessages', 'MessageContent'],
 });
 
+// greeting on start
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
   const channel = client.channels.cache.get('1257316921565646878');
@@ -55,58 +46,8 @@ client.on('messageCreate', async (message) => {
   if (!message.content.startsWith('!')) return;
   if (!hasAuthorization(message)) return;
 
-  // command list
-  const args = message.content.slice(1).trim().split(/ +/);
-  const command = args.shift().toLowerCase();
-
-  switch (command) {
-    // Remove x amount of messages from channel
-    case 'remove':
-      handleRemove(message, args);
-      break;
-
-    // Create an error to test server crash logging to AWS S3 bucket
-    case 'crash':
-      handleCreateCrash(message);
-      break;
-
-    // sprint
-    case 'sprint':
-      handleSprint(message, args, token);
-      break;
-
-    case 'userid':
-      handleUserId(message, args, token);
-      break;
-
-    // first name
-    case 'firstname':
-      handleFirstName(message, args, token);
-      break;
-
-    // last name
-    case 'lastname':
-      handleLastName(message, args, token);
-      break;
-
-    // project
-    case 'project':
-      handleProject(message, args);
-      break;
-
-    // kiitus
-    case 'kiitus':
-      let name = args.shift();
-      let kiitus = `
-${name} sa oled nii tubli! ;)`;
-      replyAndClean(message, `\`\`\`${kiitus}\`\`\``);
-      break;
-
-    // help
-    case 'help':
-      replyAndClean(message, `\`\`\`${helpInfo()}\`\`\``);
-      break;
-  }
+  // handle commands
+  handleAllCommands(message);
 });
 
 client.login(process.env.DISCORD_TOKEN);
