6089d673d3fec8dbf75ab84f136b437442a04904
jaanus <zaar2213@gmail.com>
2024-10-08 21:01:34 +0300

    fixed join/leave channel announcement bug

--- src/node/main/main.js
+++ src/node/main/main.js
@@ -50,7 +50,7 @@ client.on('messageCreate', async (message) => {
 });
 
 client.on('guildMemberRemove', async (member) => {
-  const channel = client.channels.cache.get('1257316921565646878');
+  const channel = await client.channels.fetch('1257316921565646878');
   await userLogging(member.user.tag, 'Left the server');
   channel.send(
     `\`\`\`Ahh great, someone left! Let's see now... AHA ${member.user.tag} you scoundrel, let me see where i put your credentials... What was the site again, Autolaen24.ee right?\`\`\``
@@ -58,7 +58,7 @@ client.on('guildMemberRemove', async (member) => {
 });
 
 client.on('guildMemberAdd', async (member) => {
-  const channel = client.channels.cache.get('1257316921565646878');
+  const channel = await client.channels.fetch('1257316921565646878');
   await userLogging(member.user.tag, 'Joined the server');
   channel.send(
     `\`\`\`Welcome to the server, ${member.user.tag} ! Please give me a second while i fetch your credentials to be saved for later. Toodeloo\`\`\``
